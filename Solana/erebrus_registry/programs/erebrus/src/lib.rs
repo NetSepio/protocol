1783cf467a34fdc2642e976d78c41e64dca9c044
Soumalya Paul <samratghosh240@gmail.com>
2024-11-22 23:17:50 +0530

    Solana Script

--- Solana/erebrus_registry/programs/erebrus/src/lib.rs
+++ Solana/erebrus_registry/programs/erebrus/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Bhvt1zhb14KRwXyan3cMZ52qwyyJP7Cji5HJrQrdmkAk");
 
 #[program]
 pub mod erebrus_registry {
