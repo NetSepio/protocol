59dbabc15de6e5bfc4f7e09048357447481caca9
Soumalya Paul <54816468+soumalya340@users.noreply.github.com>
2024-11-28 00:12:42 +0530

    Update lib.rs

--- erebrus_registry/programs/erebrus_registry/src/lib.rs
+++ erebrus_registry/programs/erebrus_registry/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("7XDE596AKuHKBkp333WqhSYu9u4rLrAkYThWtQJCw5La");
+declare_id!("3ypCkXQWiAFkNk7bo8bnZFxUVmVEWCqpBoY7v4vgPnHJ");
 
 #[program]
 pub mod erebrus_registry {
