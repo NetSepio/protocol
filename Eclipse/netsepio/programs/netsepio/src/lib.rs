aadc443ddad71396e4dbf3e6ccfa0e1301069f9f
Soumalya Paul <samratghosh240@gmail.com>
2025-01-19 20:58:19 +0530

    Update Eclipse Smart Contracts

--- Eclipse/netsepio/programs/netsepio/src/lib.rs
+++ Eclipse/netsepio/programs/netsepio/src/lib.rs
@@ -2,31 +2,11 @@ use anchor_lang::prelude::*;
 
 declare_id!("E5RuXgzg74bGfy8BzcvvwSUfZvXRMMU5ds1ci41gzHoz");
 
-pub const ADMIN_KEY: Pubkey =
-    solana_program::pubkey!("FG75GTSYMimybJUBEcu6LkcNqm7fkga1iMp3v4nKnDQS");
-
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
-
-pub const OFFLINE_STATUS: u8 = 0;
-pub const ONLINE_STATUS: u8 = 1;
-pub const MAINTENANCE_STATUS: u8 = 2;
 
 #[program]
 pub mod netsepio {
     use super::*;
-
-    pub fn intialize_authority(ctx: Context<IntializeAuthority>) -> Result<()> {
-        let authority = &mut ctx.accounts.authority;
-        authority.admin = ctx.accounts.user.key();
-        authority.operator = ctx.accounts.user.key();
-        Ok(())
-    }
-
-    pub fn update_authority(ctx: Context<UpdateAuthority>, operator: Pubkey) -> Result<()> {
-        let authority = &mut ctx.accounts.authority;
-        authority.operator = operator;
-        Ok(())
-    }
 
     // Registers a new node in the network with provided details and sets initial status to active (1)
     pub fn register_node(
@@ -40,6 +20,7 @@ pub mod netsepio {
         region: String,
         location: String,
         metadata: String,
+        checkpoint: String,
         owner: Pubkey,
     ) -> Result<()> {
         let node = &mut ctx.accounts.node;
@@ -52,8 +33,8 @@ pub mod netsepio {
         node.region = region;
         node.location = location;
         node.metadata = metadata;
+        node.checkpoint = checkpoint;
         node.owner = owner;
-        node.status = OFFLINE_STATUS;
 
         // emit config, metadata
         emit!(NodeRegistered {
@@ -77,76 +58,22 @@ pub mod netsepio {
         Ok(())
     }
 
-    // Updates node status to either offline (0), online (1), or maintenance (2)
-    pub fn update_node_status(
-        ctx: Context<UpdateNode>,
-        node_id: String,
-        new_status: u8,
-    ) -> Result<()> {
-        require!(
-            new_status == OFFLINE_STATUS
-                || new_status == ONLINE_STATUS
-                || new_status == MAINTENANCE_STATUS,
-            ErrorCode::InvalidStatus
-        );
-        let node = &mut ctx.accounts.node;
-        node.status = new_status; // 0: Offline, 1: Online, 2: Maintenance //
-        emit!(NodeStatusUpdated {
-            id: node_id,
-            new_status,
-        });
-
-        Ok(())
-    }
-
     // Creates a checkpoint for a node with provided data and emits timestamp in event
     pub fn create_checkpoint(
         ctx: Context<CreateCheckpoint>,
-        owner: Pubkey,
         node_id: String,
         data: String,
     ) -> Result<()> {
-        let checkpoint = &mut ctx.accounts.checkpoint;
-        checkpoint.node_id = node_id;
-        checkpoint.data = data;
+        let node = &mut ctx.accounts.node;
+        node.checkpoint = data;
 
         emit!(CheckpointCreated {
-            owner: owner.clone(),
-            node_id: checkpoint.node_id.clone(),
-            data: checkpoint.data.clone(),
+            node_id: node_id.clone(),
+            data: node.checkpoint.clone(),
         });
 
         Ok(())
     }
-}
-
-#[derive(Accounts)]
-pub struct IntializeAuthority<'info> {
-    #[account(
-        init_if_needed,
-        payer = user,
-        space = ANCHOR_DISCRIMINATOR_SIZE + Authority::INIT_SPACE,
-        seeds = [b"erebrus", user.key().as_ref()],  
-        bump,
-        constraint = user.key() == ADMIN_KEY @ ErrorCode::NotAuthorized
-    )]
-    pub authority: Account<'info, Authority>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct UpdateAuthority<'info> {
-    #[account(
-        mut,
-        seeds = [b"erebrus", user.key().as_ref()],
-        bump
-    )]
-    pub authority: Account<'info, Authority>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -156,13 +83,11 @@ pub struct RegisterNode<'info> {
         init,
         payer = user,
         space = ANCHOR_DISCRIMINATOR_SIZE + Node::INIT_SPACE,
-        seeds = [b"erebrus", id.as_bytes()],  
+        seeds = [b"netsepio", id.as_bytes()],  
         bump,
-        constraint = user.key() == authority.operator @ ErrorCode::NotAuthorized
     )]
     pub node: Account<'info, Node>,
 
-    pub authority: Account<'info, Authority>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -174,7 +99,7 @@ pub struct DeactivateNode<'info> {
     #[account(
         mut,
         close = user,
-        seeds = [b"erebrus", node_id.as_bytes()],
+        seeds = [b"netsepio", node_id.as_bytes()],
         bump,
     )]
     pub node: Account<'info, Node>,
@@ -185,31 +110,13 @@ pub struct DeactivateNode<'info> {
 
 #[derive(Accounts)]
 #[instruction(node_id: String)]
-pub struct UpdateNode<'info> {
-    #[account(
-        mut,
-        seeds = [b"erebrus", node_id.as_bytes()],
-        bump
-    )]
-    pub node: Account<'info, Node>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-#[instruction(owner: Pubkey,node_id: String)]
 pub struct CreateCheckpoint<'info> {
     #[account(
-        init_if_needed,
-        payer = user,
-        seeds = [b"erebrus",owner.as_ref(), node_id.as_bytes()],
-        space = ANCHOR_DISCRIMINATOR_SIZE + Checkpoint::INIT_SPACE,
+        mut,
+        seeds = [b"netsepio",node_id.as_bytes()],
         bump,
-        constraint = authority.operator == user.key() @ ErrorCode::NotAuthorized
     )]
-    pub checkpoint: Account<'info, Checkpoint>,
-    pub authority: Account<'info, Authority>,
+    pub node: Account<'info, Node>,
 
     #[account(mut)]
     pub user: Signer<'info>,
@@ -236,24 +143,9 @@ pub struct Node {
     pub location: String,
     #[max_len(200)]
     pub metadata: String,
-    pub owner: Pubkey,
-    pub status: u8, // 0: Offline, 1: Online, 2: Maintenance
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Checkpoint {
     #[max_len(500)]
-    pub node_id: String,
-    #[max_len(1000)]
-    pub data: String,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Authority {
-    pub admin: Pubkey,
-    pub operator: Pubkey,
+    pub checkpoint: String,
+    pub owner: Pubkey,
 }
 
 #[event]
@@ -283,15 +175,6 @@ pub struct NodeStatusUpdated {
 
 #[event]
 pub struct CheckpointCreated {
-    pub owner: Pubkey,
     pub node_id: String,
     pub data: String,
-}
-
-#[error_code]
-pub enum ErrorCode {
-    #[msg("You are not authorized to perform this action")]
-    NotAuthorized,
-    #[msg("Invalid status provided")]
-    InvalidStatus,
 }
--- Eclipse/netsepio/tests/netsepio_test.ts
+++ Eclipse/netsepio/tests/netsepio_test.ts
@@ -34,9 +34,10 @@ describe("netsepio", async () => {
     metadata: "test-metadata",
   };
 
+  const id = Math.floor(Math.random() * 1000000).toString();
+  console.log("Id: ", id);
   // Test data 2
   const testNode2 = {
-    id: "test-node-2",
     name: "Test Node 2",
     nodeType: "validator 2",
     config: "test-config-2",
@@ -84,99 +85,16 @@ describe("netsepio", async () => {
   const operator = anchor.web3.Keypair.generate();
   const user = anchor.web3.Keypair.generate();
 
-  const [authorityPDA] = PublicKey.findProgramAddressSync(
-    [Buffer.from("erebrus"), admin.publicKey.toBuffer()],
-    program.programId
-  );
-
-  it("Should not allow non-admin to initialize authority", async () => {
-    console.log("Transferring SOL to user ..... 🚚🚚");
-    await transferSol(user.publicKey);
-    console.log("Initializing authority");
-    try {
-      await program.methods
-        .intializeAuthority()
-        .accounts({
-          user: user.publicKey,
-        })
-        .signers([user])
-        .rpc();
-      assert(false, "should've failed but didn't ");
-    } catch (error) {
-      console.log("Message: ", error.message);
-    }
-  });
-
-  it("Should successfully initialize authority when called by admin", async () => {
-    // Initialize authority
-    const tx = await program.methods
-      .intializeAuthority()
-      .accounts({
-        user: admin.publicKey,
-      })
-      .rpc();
-    console.log("Transaction Signature", tx);
-
-    // Fetch the created authority account
-    const authorityAccount = await program.account.authority.fetch(
-      authorityPDA
-    );
-    // Verify the authority was set correctly
-    assert.equal(authorityAccount.admin.toBase58(), admin.publicKey.toBase58());
-    assert.equal(
-      authorityAccount.operator.toBase58(),
-      admin.publicKey.toBase58()
-    );
-
-    console.log("✅ Authority initialized successfully by admin");
-  });
-
-  it("Should update authority", async () => {
-    await program.methods.updateAuthority(operator.publicKey).rpc();
-    console.log("Successfully updated authority 🎉");
-    const [authorityPDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), admin.publicKey.toBuffer()],
-      program.programId
-    );
-    const authorityAccount = await program.account.authority.fetch(
-      authorityPDA
-    );
-    assert.equal(
-      authorityAccount.operator.toBase58(),
-      operator.publicKey.toBase58()
-    );
-  });
-
-  it("Violate Update Authority", async () => {
-    try {
-      await program.methods
-        .updateAuthority(user.publicKey)
-        .accounts({
-          user: user.publicKey,
-        })
-        .rpc();
-      console.log("It should have failed but didn't");
-    } catch (error) {
-      console.log("Message : ", error.message);
-    }
-  });
-
   // Add these to your existing test file
-  it("Should register node when called by operator", async () => {
+  it("Should register node when called ", async () => {
     console.log("Transferring SOL to user ..... 🚚🚚");
     await transferSol(operator.publicKey);
     console.log("SOL transferred successfully");
 
     console.log("Registering node by operator...");
-    let id = Math.floor(Math.random() * 1000000).toString();
-    console.log("Id: ", id);
 
-    const [authorityPDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), admin.publicKey.toBuffer()],
-      program.programId
-    );
     const [nodePDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), Buffer.from(id)],
+      [Buffer.from("netsepio"), Buffer.from(id)],
       program.programId
     );
 
@@ -192,11 +110,11 @@ describe("netsepio", async () => {
         testNode.region,
         testNode.location,
         testNode.metadata,
+        "first checkpoint",
         user.publicKey // owner
       )
       .accounts({
         user: operator.publicKey,
-        authority: authorityPDA,
       })
       .signers([operator])
       .rpc();
@@ -214,11 +132,11 @@ describe("netsepio", async () => {
     assert.equal(nodeAccount.location, testNode.location);
     assert.equal(nodeAccount.metadata, testNode.metadata);
     assert.equal(nodeAccount.owner.toBase58(), user.publicKey.toBase58());
-    assert.equal(nodeAccount.status, 0);
+    assert.equal(nodeAccount.checkpoint, "first checkpoint");
   });
   it("Should not allow duplicate node registration", async () => {
     const [authorityPDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), admin.publicKey.toBuffer()],
+      [Buffer.from("netsepio"), admin.publicKey.toBuffer()],
       program.programId
     );
     const nodeAddress = anchor.web3.Keypair.generate().publicKey;
@@ -234,11 +152,11 @@ describe("netsepio", async () => {
           testNode.region,
           testNode.location,
           testNode.metadata,
+          "first checkpoint",
           user.publicKey
         )
         .accounts({
           user: operator.publicKey,
-          authority: authorityPDA,
         })
         .signers([operator])
         .rpc();
@@ -247,114 +165,45 @@ describe("netsepio", async () => {
       console.log("Already Intialized before");
     }
   });
-  it("Should not allow non-operator node registration", async () => {
-    const [authorityPDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), admin.publicKey.toBuffer()],
-      program.programId
-    );
-    const nodeAddress = anchor.web3.Keypair.generate().publicKey;
-
-    try {
-      await program.methods
-        .registerNode(
-          testNode2.id,
-          nodeAddress,
-          testNode2.name,
-          testNode2.nodeType,
-          testNode2.config,
-          testNode2.ipaddress,
-          testNode2.region,
-          testNode2.location,
-          testNode2.metadata,
-          user.publicKey
-        )
-        .accounts({
-          user: user.publicKey,
-          authority: authorityPDA,
-        })
-        .signers([user])
-        .rpc();
-      console.log("It should have failed but didn't");
-    } catch (error) {
-      console.log("Non Operator cannot register node");
-    }
-  });
-  it("Should update node status", async () => {
-    await program.methods
-      .updateNodeStatus(testNode.id, 1)
-      .accounts({
-        user: operator.publicKey,
-      })
-      .signers([operator])
-      .rpc();
-    console.log("Node status updated successfully");
-    const [nodePDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), Buffer.from(testNode.id)],
-      program.programId
-    );
-    const nodeAccount = await program.account.node.fetch(nodePDA);
-    assert.equal(nodeAccount.status, 1);
-  });
-
-  it("Should not allow invalid status update", async () => {
-    try {
-      await program.methods
-        .updateNodeStatus(testNode.id, 10)
-        .accounts({
-          user: operator.publicKey,
-        })
-        .signers([operator])
-        .rpc();
-      console.log("It should have failed but didn't");
-    } catch (error) {
-      console.log("Invalid status update");
-    }
-  });
-
   it(" Should create checkpoint", async () => {
+    const [nodePDA] = PublicKey.findProgramAddressSync(
+      [Buffer.from("netsepio"), Buffer.from(id)],
+      program.programId
+    );
     const data = "test-data";
+    console.log("Creating checkpoint");
+    console.log("Id for node: ", id);
     await program.methods
-      .createCheckpoint(user.publicKey, testNode.id, data)
+      .createCheckpoint(id, data)
       .accounts({
         user: operator.publicKey,
-        authority: authorityPDA,
       })
       .signers([operator])
       .rpc();
     console.log("Checkpoint 1 created successfully");
-    const [checkpointPDA] = PublicKey.findProgramAddressSync(
-      [
-        Buffer.from("erebrus"),
-        user.publicKey.toBuffer(),
-        Buffer.from(testNode.id),
-      ],
-      program.programId
-    );
-    const checkpointAccount = await program.account.checkpoint.fetch(
-      checkpointPDA
-    );
-    assert.equal(checkpointAccount.data, data);
+    const checkpointAccount = await program.account.node.fetch(nodePDA);
+    assert.equal(checkpointAccount.checkpoint, data);
     console.log("Again giving data");
     const data2 = "test-data2";
     await program.methods
-      .createCheckpoint(user.publicKey, testNode.id, data2)
+      .createCheckpoint(id, data2)
       .accounts({
         user: operator.publicKey,
-        authority: authorityPDA,
       })
       .signers([operator])
       .rpc();
     console.log("Checkpoint 2 created successfully");
-    const checkpointAccount2 = await program.account.checkpoint.fetch(
-      checkpointPDA
-    );
-    assert.equal(checkpointAccount2.data, data2);
+    const checkpointAccount2 = await program.account.node.fetch(nodePDA);
+    assert.equal(checkpointAccount2.checkpoint, data2);
   });
   it("Should not allow non-owner to create checkpoint", async () => {
     const data = "test-data1";
     try {
       await program.methods
-        .createCheckpoint(user.publicKey, testNode.id, data)
+        .createCheckpoint(testNode.id, data)
+        .accounts({
+          user: user.publicKey,
+        })
         .signers([user])
         .rpc();
       console.log("It should have failed but didn't");
@@ -364,11 +213,11 @@ describe("netsepio", async () => {
   });
   it("Deactivate Node", async () => {
     const nodeAddress = anchor.web3.Keypair.generate().publicKey;
-    let id = Math.floor(Math.random() * 1000000).toString();
-    console.log("Id: ", id);
+    let id2 = Math.floor(Math.random() * 1000000).toString();
+    console.log("Id: ", id2);
     await program.methods
       .registerNode(
-        id,
+        id2,
         nodeAddress,
         testNode2.name,
         testNode2.nodeType,
@@ -377,17 +226,17 @@ describe("netsepio", async () => {
         testNode2.region,
         testNode2.location,
         testNode2.metadata,
+        "first checkpoint",
         user.publicKey
       )
       .accounts({
         user: operator.publicKey,
-        authority: authorityPDA,
       })
       .signers([operator])
       .rpc();
     console.log("New Node registered successfully");
     const [nodePDA] = PublicKey.findProgramAddressSync(
-      [Buffer.from("erebrus"), Buffer.from(id)],
+      [Buffer.from("netsepio"), Buffer.from(id)],
       program.programId
     );
     const nodeAccount = await program.account.node.fetch(nodePDA);
